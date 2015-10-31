a6b9ff05c92eca7fba64d95f94afaeee397cb91c
Darryl Benton Dixon <piecedigitalstudios@gmail.com>
2015-10-31 06:23:38 -0400

    added scroll effect

--- js/main.js
+++ js/main.js
@@ -36,12 +36,63 @@ $(document).ready(function() {
 		return false;
 	};
 
-	$(document).on("click", function(e) {
+	$(document).on("click", "a", function(e) {
+		var id = $(this).attr("href");
+		if(!id.match(/#[a-z0-9\-_]*/i)) {
+			return false;
+		};
+		document.body.offsetHeight = document.body.offsetHeight ||
+																document.body.innerHeight ||
+																document.getElementsByTagName("body")[0].offsetHeight;
 
-		/*var scroll = $(this).scrollTop();
-		$(this).scrollTop( scroll + 800);*/
+		document.body.clientHeight = document.body.clientHeight ||
+																document.getElementsByTagName("body")[0].clientHeight;
 
-		//document.body.scrollTop = 500;
-		console.log(document.body.scrollTop);
+		console.log(document.body.scrollTop, document.body.offsetHeight, document.body.clientHeight)
+		if($(id).offset().top > document.body.scrollTop + 100) {
+			var scrollDown = function(ind, inc, max) {
+				ind = ind || 0;
+
+				if(document.body.scrollTop < $(id).offset().top - 50 &&
+				document.body.scrollTop < (document.body.offsetHeight - document.body.clientHeight) - 100) {
+					setTimeout(function() {
+						var num = inc;
+						document.body.scrollTop += num;
+
+						console.log(num);
+
+						scrollDown(ind+1, inc, max);
+					}, 0);
+				} else {
+					scrollDown = null;
+				}
+			};
+
+			var dist = $(id).offset().top - document.body.scrollTop;
+			scrollDown(0, dist / 20, 200);
+		} else
+		if($(id).offset().top < document.body.scrollTop + 100) {
+			var scrollUp = function(ind, inc, max) {
+				ind = ind || 0;
+
+				if(document.body.scrollTop > $(id).offset().top + 50 &&
+				document.body.scrollTop > 0 + 50) {
+					setTimeout(function() {
+						var num = inc;
+						document.body.scrollTop -= num;
+
+						console.log(num);
+
+						scrollUp(ind+1, inc, max);
+					}, 0);
+				} else {
+					scrollUp = null;
+				}
+			};
+
+			var dist = document.body.scrollTop - $(id).offset().top;
+			scrollUp(0, dist / 20, 200);
+		}
+		return false;
 	});
 });
